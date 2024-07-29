717d3e3523bb497fc866b38178ca876ffd524b70
Kylin_on_Mac <kylingithubdev@gmail.com>
2024-07-29 15:44:15 +0800

    feat:update gif

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import sys
 from os import path
 sys.path.append(path.dirname(__file__))
 
-from src.game import SwimmingSquid
+from src.game import SwimmingSquidBattle
 
 GAME_SETUP = {
-    "game": SwimmingSquid,
+    "game": SwimmingSquidBattle,
     # "dynamic_ml_clients":True
 }
