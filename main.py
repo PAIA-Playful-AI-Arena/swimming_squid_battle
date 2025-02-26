e6b5a33bed1ac0e28e7c6f4023c1017df74bdeee
Kylin_on_Mac <kylingithubdev@gmail.com>
2025-02-26 15:26:04 +0800

    feat:update main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import sys
 
 from mlgame.view.view import PygameView
 from mlgame.game.generic import quit_or_esc
@@ -12,8 +11,9 @@ if __name__ == '__main__':
     scene_init_info_dict = game.get_scene_init_data()
     game_view = PygameView(scene_init_info_dict)
     frame_count = 0
+    clock = pygame.time.Clock()
     while game.is_running and not quit_or_esc():
-        pygame.time.Clock().tick_busy_loop(FPS)
+        clock.tick(FPS)
         commands = game.get_keyboard_command()
         game.update(commands)
         game_progress_data = game.get_scene_progress_data()
